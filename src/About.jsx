b7cce069bbebff362bc77625dc32e91922c22748
Moosa Raheel <moosarahil13@gmail.com>
2023-07-19 06:10:45 +0500

    About Page Complete using Hero component by props

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import HeroSection from "./components/HeroSection";
+import aboutImage from "./assets/about.svg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <HeroSection head={"Moosa Raheel"} image={aboutImage} />
+    </>
+  );
 };
 
 export default About;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
+import { Link } from "react-router-dom";
+export default function HeroSection({ head, image }) {
+  return (
+    <Wrapper>
+      <div className="container grid">
+        <div className="hero-cont">
+          <p className="top-para">Hy It's me</p>
+          <h1 className="hero-head">{head}</h1>
+          <p className="hero-para">
+            I am a Full Stack Web Application Developer, Youtuber and Free
+            lancer
+          </p>
+          <Button>
+            <Link to="/contact">Hire Me</Link>
+          </Button>
+        </div>
+        <div className="hero-img">
+          <picture>
+            <img src={image} alt="Developer" />
+          </picture>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+const Wrapper = styled.section`
+  margin-top: 4em;
+  div.grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
+    gap: 3em;
+  }
+  div.hero-img img {
+    width: 100%;
+    height: 35rem;
+  }
+  .top-para {
+    font-size: 1.7rem;
+    font-weight: 500;
+  }
+  .hero-para {
+    font-size: 1.8rem;
+  }
+`;
